b1f0f09d81b922f0ed8ec9b1fc67512746f8000f
Odin391 <denifedulov@gmail.com>
2026-05-01 14:27:56 +0300

    5.0

--- Scripts.Deni/Gamer.gd
+++ Scripts.Deni/Gamer.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 var speed = 40
 
+@onready var koffeBall = preload("res://Scripts.Deni/bullet.tscn")
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("LMB"):
+		_spawn()
 
 
 
@@ -12,5 +17,10 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("Shift"):
 		velocity *= 2
 	
-	
 	move_and_slide()
+
+
+func _spawn():
+	var bullet = koffeBall.instantiate()
+	bullet.position = self.global_position
+	get_parent().add_child(bullet)
--- Scripts.Deni/bullet.gd
+++ Scripts.Deni/bullet.gd
@@ -1,11 +1,21 @@
 extends RigidBody2D
 
 var in_action = false
-
+var damage = 10
+@onready var timer = get_node("Timer")
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("LMB") and in_action == false:
 		in_action = true
 		var target = get_global_mouse_position()
 		var direction = (target - position).normalized()
-		apply_central_force(direction * 40)
+		apply_force(direction * 3000)
+
+
+
+func _on_timer_timeout() -> void:
+	damage -= 2
+	var tween = create_tween()
+	tween.tween_property(self, "scale", scale * 0.8, 1)
+	if damage <= 0:
+		queue_free()
