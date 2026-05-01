c1a5b2cb33872b408b0318ab1362147b14d0a90f
Odin391 <denifedulov@gmail.com>
2026-05-01 09:36:55 +0300

    3.0

--- Scripts.Deni/Gamer.gd
+++ Scripts.Deni/Gamer.gd
@@ -1,4 +1,13 @@
 extends CharacterBody2D
 
+var speed = 40
+
 func _process(delta: float) -> void:
-	print (velocity)
+	velocity.y = Input.get_axis("W", "S") * speed
+	velocity.x = Input.get_axis("A", "D") * speed
+	
+	if Input.is_action_pressed("Shift"):
+		velocity *= 2
+	
+	
+	move_and_slide()
