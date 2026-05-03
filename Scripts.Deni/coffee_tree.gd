f87390c675d1faba7c74205c6f1ffc03969d78d8
Odin391 <denifedulov@gmail.com>
2026-05-03 16:44:50 +0300

    10.7

--- Scripts.Deni/coffee_tree.gd
+++ Scripts.Deni/coffee_tree.gd
@@ -2,7 +2,21 @@ extends Node2D
 
 var is_in_area = false
 
-func _on_tree_2d_body_entered(body: Node2D) -> void:
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body as Gamer:
 		is_in_area = true
-	
+		$Label.visible = true
+
+func  _process(delta: float) -> void:
+	if is_in_area == true:
+		if Input.is_action_just_pressed("RMB"):
+			queue_free()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body as Gamer:
+		is_in_area = false
+		$Label.visible = false
